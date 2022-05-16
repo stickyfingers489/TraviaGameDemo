6162f99d42d01569f98f23315b9093ae05316863
stickyfingers489 <mutuadennis56@gmai.com>
2022-05-16 15:00:24 +0300

    Initial Commit

---
+++ TriviaGameDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TriviaGameDemo
+//
+//  Created by stickerfinger489 on 16/05/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TriviaGameDemo/TriviaGameDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TriviaGameDemoApp.swift
+//  TriviaGameDemo
+//
+//  Created by stickerfinger489 on 16/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct TriviaGameDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
